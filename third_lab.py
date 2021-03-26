2427593dea673a328a2537a38aa375a362f5e26e
Artem <artem-budakov@mail.ru>
2021-03-26 18:06:29 +0300

    added third task for functions

--- third_lab.py
+++ third_lab.py
@@ -2,12 +2,24 @@ import math
 
 
 def distance(x1: float = 0, y1: float = 0, x2: float = 0, y2: float = 0):
-    # print(f"distance between x1, y1 and x2, y2: {math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)}")
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 
 def miles_to_km(miles: float = 0):
     return miles * 1.609
+
+
+def mean(args):
+    """
+    # equals
+    average, counter = 0, 0
+    for arg in args:
+        average += arg
+        counter += 1
+    return average / counter
+    """
+
+    return sum(args) / len(args)
 
 
 if __name__ == '__main__':
@@ -16,9 +28,12 @@ if __name__ == '__main__':
     # result = distance(x1=values_list[0], y1=values_list[1], x2=values_list[2], y2=values_list[3])
     # print(str(result))
 
-    value = float(input("write miles in float: "))
-    result = miles_to_km(value)
+    # value = float(input("write miles in float: "))
+    # result = miles_to_km(value)
+    # print(str(result))
 
+    values = [float(value) for value in (input("write numbers: ")).split()]
+    result = mean(values)
     print(str(result))
 
 
