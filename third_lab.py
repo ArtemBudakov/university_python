b52c16753561b370304b1099c9bb13db0a38d771
Artem <artem-budakov@mail.ru>
2021-03-26 18:41:09 +0300

    finished functions for now. have problems with last to tasks.

--- third_lab.py
+++ third_lab.py
@@ -31,22 +31,33 @@ def concat(args):
     return result
 
 
+def call(function, args):
+    """Strange task. I don't know how i must do it and why someone use it. skipped for now.
+    https://stackoverflow.com/questions/40644740/what-is-the-difference-between-the-apply-function-and-a-function-call-using-th"""
+    pass
+
+
 if __name__ == '__main__':
 
+    # first task - distance()
     # values_list = [float(value) for value in (input("write x1, y1 and x2, y2: ")).split()]
     # result = distance(x1=values_list[0], y1=values_list[1], x2=values_list[2], y2=values_list[3])
     # print(str(result))
 
+    # second task - miles_to_km()
     # value = float(input("write miles in float: "))
     # result = miles_to_km(value)
     # print(str(result))
 
+    # third task - mean()
     # values = [float(value) for value in (input("write numbers: ")).split()]
     # result = mean(values)
     # print(str(result))
 
-    values = input("write numbers: ").split()
-    print(concat(values))
+    # fourth task - concat()
+    # values = input("write numbers: ").split()
+    # print(concat(values))
+    pass
 
 
 
