0016cee3d58fe7f9e0684cd3ae66a8c8448ec27b
Artem <artem-budakov@mail.ru>
2021-05-24 17:56:41 +0300

    added descriptions for tasks

--- second_lab.py
+++ second_lab.py
@@ -4,12 +4,16 @@ import random
 class Lists:
 
     def one(self):
+        """Сгенерировать список из 10 последовательных чисел от 1 до 10.
+        Вывести на экран все элементы с нечетными индексами"""
         my_list = [element for element in range(1, 10)]
         for index, value in enumerate(my_list):
             if index % 2:
                 print(my_list[index])
 
     def two(self):
+        """Сгенерировать список из 20 чисел из любого диапазона.
+        Вывести на экран все четные элементы и их индексы"""
         my_list: list = []
 
         while len(my_list) < 20:
@@ -20,6 +24,11 @@ class Lists:
                 print(f" el = {my_list[index]}, index = {index}")
 
     def three(self):
+        """Сгенерировать список из 10 случайных целых чисел из любого диапазона (для генерации случайных
+        элементов использовать модуль random
+        https://pythonworld.ru/moduli/modul-random.html)
+        Выведете список на экран. Отсортируйте список по возрастанию и по убыванию.
+        Выведете на экран оба отсортированных списка."""
         my_list: list = []
 
         while len(my_list) < 10:
@@ -32,6 +41,8 @@ class Lists:
         print(f"sorted desc list: {my_list}")
 
     def four(self):
+        """Сгенерировать список из 10 случайных целых чисел из любого диапазона Выведете список на экран.
+        Выведите все элементы этого списка, которые больше предыдущего элемента"""
         my_list: list = []
 
         while len(my_list) < 10:
@@ -46,6 +57,10 @@ class Lists:
                 compare = el
 
     def five(self):
+        """Сгенерировать список из 10 случайных целых чисел из любого диапазона.
+        Выведете список на экран. Выведите значение наибольшего элемента в списке,
+        а затем индекс этого элемента в списке.
+        Если наибольших элементов несколько, выведите индекс первого из них."""
         my_list: list = []
 
         while len(my_list) < 10:
@@ -63,6 +78,10 @@ class Lists:
         print(f"element = {max_el}, index = {max_el_index}")
 
     def six(self):
+        """Сгенерировать список из 10 случайных целых чисел из любого диапазона.
+        Выведете список на экран. Отсортируйте список по возрастанию,
+        переставьте соседние элементы списка (A[0] c A[1], A[2] c A[3] и т. д.).
+        Если элементов нечетное число, то последний элемент остается на своем месте"""
         my_list: list = []
 
         while len(my_list) < 10:
@@ -80,6 +99,8 @@ class Lists:
         print(f"changed list: {my_list}\n")
 
     def seven(self):
+        """Сгенерировать список из 10 случайных целых чисел из любого диапазона. Выведете список на экран.
+        Поменяйте местами минимальный и максимальный элемент этого списка."""
         my_list: list = []
 
         while len(my_list) < 10:
@@ -105,6 +126,15 @@ class Lists:
 class Strings:
 
     def one(self):
+        """Введите строку с клавиатуры. Сначала выведите третий символ этой строки.
+        Во второй строке выведите предпоследний символ этой строки.
+        В третьей строке выведите первые пять символов этой строки.
+        В четвертой строке выведите всю строку, кроме последних двух символов.
+        В пятой строке выведите все символы с четными индексами (считая, что индексация начинается с 0, поэтому символы выводятся начиная с первого).
+        В шестой строке выведите все символы с нечетными индексами, то есть начиная со второго символа строки.
+        В седьмой строке выведите все символы в обратном порядке.
+        В восьмой строке выведите все символы строки через один в обратном порядке, начиная с последнего.
+        В девятой строке выведите длину данной строки"""
         string_entered = str(input("write a message: "))
         print(f"third symbol: {string_entered[2]}\n"
               f"before last symbol: {string_entered[-2]}\n"
@@ -117,11 +147,16 @@ class Strings:
               f"current length: {len(string_entered)}")
 
     def two(self):
+        """Введите строку с клавиатуры, состоящую из слов, разделенных пробелами.
+        Определите, сколько в ней слов."""
         string_entered = str(input("write a message: "))
 
         print(f"words: {len(string_entered.split())}")
 
     def three(self):
+        """Введите строку с клавиатуры. Разрежьте ее на две равные части (если длина строки — четная,
+        а если длина строки нечетная, то длина первой части должна быть на один символ больше).
+        Переставьте эти две части местами, результат запишите в новую строку и выведите на экран."""
         string_entered = str(input("write a message: "))
         if len(string_entered) % 2 == 0:
             print(f"half and half: {string_entered[len(string_entered) // 2:]} and "
@@ -131,11 +166,16 @@ class Strings:
                   f"{string_entered[:len(string_entered) // 2 + 1]}")
 
     def four(self):
+        """Введите строку с клавиатуры, состоящую ровно из двух слов, разделенных пробелом.
+        Переставьте эти слова местами. Результат запишите в строку и выведите получившуюся строку."""
         string_entered = str(input("write a message using only two words with space between: "))
 
         print(f"words: {string_entered[string_entered.find(' ') + 1:]} and {string_entered[:string_entered.find(' ')]}")
 
     def five(self):
+        """Введите строку с клавиатуры. Если в этой строке буква f встречается только один раз, выведите её индекс.
+        Если она встречается два и более раз, выведите индекс её первого и последнего появления.
+        Если буква f в данной строке не встречается, ничего не выводите."""
         string_entered = str(input("write a message: "))
         if string_entered.count("f") == 1:
             print(f'only one: {string_entered.find("f")}')
@@ -143,10 +183,13 @@ class Strings:
             print(f'only twice: {string_entered.find("f"), string_entered.rfind("f")}')
 
     def six(self):
+        """Введите строку с клавиатуры, в которой буква h встречается минимум два раза.
+        Удалите из этой строки первое и последнее вхождение буквы h, а также все символы, находящиеся между ними"""
         string_entered = str(input("write a message: "))
         print(f"Output: {string_entered[:string_entered.find('h')] + string_entered[string_entered.rfind('h') + 1:]}")
 
     def seven(self):
+        """Введите строку с клавиатуры. Замените в этой строке все цифры 1 на слово one"""
         string_entered = str(input("write a message: "))
         print(f"'one' instead '1': {string_entered.replace('1', 'one')}")
 
@@ -154,6 +197,8 @@ class Strings:
 class Dicts:
 
     def one(self):
+        """Введите строку с клавиатуры. Посчитайте, сколько раз каждое слово в строке встречалось ранее.
+        Словом считается последовательность непробельных символов идущих подряд, слова разделены одним или большим числом пробелов или символами конца строки"""
         string_entered = str(input("write a message: "))
         counter: dict = {}
 
@@ -167,6 +212,8 @@ class Dicts:
             print(f"{word} {counter[word]}")
 
     def two(self):
+        """Введите строку с клавиатуры.
+        Для каждого слова из данного текста подсчитайте, сколько раз оно встречалось в этом тексте ранее."""
         string_entered = str(input("write a message: "))
         counter: dict = {}
 
@@ -179,6 +226,9 @@ class Dicts:
                 print(f"{el} - {counter[el]}")
 
     def three(self):
+        """Введите строку с клавиатуры. Составьте словарь, состоящий из уникальных слов, содержащихся в этой строке.
+        Выведете на экран последовательность из индексов (ключей) этого словаря, в том порядке,
+        в котором они встречаются в строке"""
         string_entered = str(input("write a message: "))
         counter_dict: dict = {}
 
@@ -198,6 +248,9 @@ class Dicts:
             print(f"{word} {value}")
 
     def four(self):
+        """Составьте «словарь синонимов» следующим образом: сначала введите количество вводимых пар ключ-значение.
+        Каждое слово является синонимом к парному ему слову.
+        Затем введите слово, для которого надо вывести синоним"""
         counter_message = int(input("write count of message: "))
         counter_dict: dict = {}
 
@@ -210,6 +263,7 @@ class Dicts:
         print(f"antonym: {counter_dict[str(input('write a word: '))]}")
 
     def five(self):
+        """Введите строку с клавиатуры. Подсчитайте количество появлений каждого символа в строке"""
         string_entered = str(input("write a message: "))
         string_entered = string_entered.replace(" ", "")
         counter_dict: dict = {}
@@ -224,7 +278,7 @@ class Dicts:
             print(f"{word} {counter_dict[word]}")
 
     def six(self):
-        """earlier and more often"""
+        """Введите строку с клавиатуры. Выведите слово, которое в этом тексте встречается чаще всего."""
         string_entered = str(input("write a message: "))
         counter_dict: dict = {}
 
